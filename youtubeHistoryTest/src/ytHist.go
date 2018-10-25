809ba8be3d316044ad09cd1417e2731c59955c90
EdgarBodiaj <43748334+EdgarBodiaj@users.noreply.github.com>
2018-10-25 09:51:01 +0100

    Add files via upload

--- youtubeHistoryTest/src/ytHist.go
+++ youtubeHistoryTest/src/ytHist.go
@@ -44,12 +44,14 @@ type Video struct {
 
 func main() {
 
+	DataboxTestMode := os.Getenv("DATABOX_VERSION") == ""
+
 	//The endpoints and routing for the UI
 	router := mux.NewRouter()
 	router.HandleFunc("/status", statusEndpoint).Methods("GET")
 	router.HandleFunc("/ui/info", infoUser)
 	router.PathPrefix("/ui").Handler(http.StripPrefix("/ui", http.FileServer(http.Dir("./static"))))
-	setUpWebServer(false, router, "8080")
+	setUpWebServer(DataboxTestMode, router, "8080")
 }
 
 func infoUser(w http.ResponseWriter, r *http.Request) {
@@ -66,18 +68,27 @@ func infoUser(w http.ResponseWriter, r *http.Request) {
 		}
 
 	}
+	fmt.Fprintf(w, "<h1>Working!!!!<h1>")
 	go doDriverWork(username, password)
 }
 
 func doDriverWork(username string, password string) {
-
+	DataboxTestMode := os.Getenv("DATABOX_VERSION") == ""
 	libDatabox.Info("Starting ....")
 
-	var DataboxStoreEndpoint string
+	DataboxStoreEndpoint := "tcp://127.0.0.1:5555"
 	var storeClient *libDatabox.CoreStoreClient
 
-	DataboxStoreEndpoint = os.Getenv("DATABOX_STORE_ENDPOINT")
-	storeClient = libDatabox.NewDefaultCoreStoreClient(DataboxStoreEndpoint)
+	if DataboxTestMode {
+		DataboxStoreEndpoint = testStoreEndpoint
+		ac, _ := libDatabox.NewArbiterClient("./", "./", testArbiterEndpoint)
+		storeClient = libDatabox.NewCoreStoreClient(ac, "./", DataboxStoreEndpoint, false)
+		//turn on debug output for the databox library
+		libDatabox.OutputDebug(true)
+	} else {
+		DataboxStoreEndpoint = os.Getenv("DATABOX_ZMQ_ENDPOINT")
+		storeClient = libDatabox.NewDefaultCoreStoreClient(DataboxStoreEndpoint)
+	}
 
 	libDatabox.Info("starting doDriverWork")
 
@@ -101,34 +112,36 @@ func doDriverWork(username string, password string) {
 	libDatabox.Info("Registered Datasource")
 
 	cmdName := "youtube-dl"
+	tempUse := "-u " + username
+	tempPas := "-p " + password
+	cmdArgs := []string{tempUse, tempPas,
+		"--skip-download",
+		"--dump-single-json",
+		"--playlist-items",
+		"1-10",
+		"https://www.youtube.com/feed/history"}
 
-	tempUse := "-p " + password
-	tempPas := "-u " + username
 	//Create recent store
 	var hOld Playlist
 	for {
 		//Create new var for incoming data
 		var hNew Playlist
-		cmdArgs := []string{tempUse, tempPas,
-			"--skip-download",
-			"-o'%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'",
-			"--dump-single-json",
-			"--playlist-items",
-			"1-10",
-			"https://www.youtube.com/feed/history"}
-		if cmdOut, er = exec.Command(cmdName, cmdArgs[0], cmdArgs[1], cmdArgs[2], cmdArgs[3], cmdArgs[4], cmdArgs[5], cmdArgs[6], cmdArgs[7]).Output(); er != nil {
-			fmt.Println("er")
+		if cmdOut, er = exec.Command(cmdName, cmdArgs[0], cmdArgs[1], cmdArgs[2], cmdArgs[3], cmdArgs[4], cmdArgs[5], cmdArgs[6]).Output(); er != nil {
+			fmt.Println(er.Error())
+			return
+		}
+		libDatabox.Info("Download Finished")
+		err := json.Unmarshal(cmdOut, &hNew)
+		if err != nil {
+			fmt.Println(err.Error())
 			return
 		}
 
-		err := json.Unmarshal(cmdOut, &hNew)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
 		//Check to see if the recent store is populated
 		//If it has been populated, compare new items with the stored items
 		if hOld.Item != nil {
+			fmt.Println("New size " + string(len(hNew.Item)))
+			fmt.Println("Old size " + string(len(hOld.Item)))
 			for i := 0; i < len(hNew.Item); i++ {
 				for j := 0; j < len(hOld.Item); j++ {
 					//If a duplicate is found in the recent store, do not save item
@@ -139,7 +152,7 @@ func doDriverWork(username string, password string) {
 					if j == len(hOld.Item)-1 {
 						temp, tErr := json.Marshal(hNew.Item[i])
 						if tErr != nil {
-							fmt.Println("" + tErr.Error())
+							fmt.Println(tErr.Error())
 							return
 						}
 						aerr := storeClient.TSBlobJSON.Write("YoutubeHistory", temp)
@@ -147,6 +160,7 @@ func doDriverWork(username string, password string) {
 							libDatabox.Err("Error Write Datasource " + aerr.Error())
 						}
 						//libDatabox.Info("Data written to store: " + string(temp))
+						fmt.Println("Storing data")
 						libDatabox.Info("Storing data")
 					}
 				}
